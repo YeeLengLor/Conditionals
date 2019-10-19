a9fa109845689df1c85a6e1615f7d39f65127ff0
YeeLengLor <55327087+YeeLengLor@users.noreply.github.com>
2019-10-19 14:26:40 -0500

    Initial commit

---
+++ practiceConditional.py
@@ -0,0 +1,19 @@
+'''
+Created on Oct 19, 2019
+
+@author: ITAUser
+'''
+x = -1
+if x > 0 :
+    print("The X value is positive")
+elif x==0:
+    print("The X value is zero")
+    
+    print("The X value is negative")  
+
+
+
+
+
+
+
